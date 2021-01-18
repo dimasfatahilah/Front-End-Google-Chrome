2c9d5c122507cffa6b6fdc25b09787678fb125a2
Dimas Fatahilah <75323182+dimasfatahilah@users.noreply.github.com>
2021-01-18 10:42:30 +0700

    Create Pengumpulan.java

---
+++ Pengumpulan.java
@@ -0,0 +1,14 @@
+package com.example.googleclassroom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pengumpulan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pengumpulan);
+    }
+}
